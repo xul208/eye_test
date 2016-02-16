a54210807ed64c5af8cc9aad313d862739de1f27
Eric Xu Li <lixulehigh@gmail.com>
2016-02-16 07:56:50 -0500

    timeouts

--- popup.js
+++ popup.js
@@ -1,1 +1,17 @@
-chrome.tabs.update(null, {url:"http://en.wikipedia.org"});
+var domains = ['com', 'ca', 'com.br'];
+
+function getCurrentDomain() {
+  var currentUrl = chrome.tabs.getCurrent.url;
+  return currentUrl;
+}
+
+function nextStep () {
+  //chrome.tabs.update(null, {url:'https://xli-dev.dhcp.tripadvisor.ru'});
+  console.log(getCurrentDomain());
+
+  //setTimeout(nextStep2, 3000);
+  //console.log(0);
+}
+
+// document.getElementById('clickme').addEventListener('click', test); 
+setTimeout(nextStep, 3000);
